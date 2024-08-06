3a13e556856a3059ec09e37fde386ecb952e0c50
AdrakPro <adrak99.adrak99@gmail.com>
2024-08-06 14:58:07 +0200

    Restructure

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/libs/**/*.{js,ts,svelte}',
+    './src/routes/**/*.{js,ts,svelte}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
